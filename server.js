0b06fdb3857d17105c92bb151eca3b391ba6a713
E-Burton <edwina.antoinette@gmail.com>
2021-02-28 17:31:49 -0700

    Moved all folders/files to root directory and deleted Develop directory.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+var PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -13,17 +13,20 @@ app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-mongoose.connect("mongodb://localhost/budget-tracker", {
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/budget-tracker',
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false
-});
+  }
+);
 
 // routes
-app.use(require("./Develop/routes/api.js"));
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
